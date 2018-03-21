9a80e09c444208ec90e69bc08e87cd39b9b5c38b
theRealGS <1065047855@qq.com>
2018-03-21 21:58:22 +0800

    first commit

---
+++ XFrameworkServer/XFrameworkServer/Servers/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace XFrameworkServer.Servers
+{
+    /// <summary>
+    /// 客户端类
+    /// 用于存储连接到服务器的客户端 接收、发送消息
+    /// </summary>
+    class Client
+    {
+        private Socket m_socket;
+
+        public Client() { }
+        public Client(Socket clientSocket)
+        {
+            this.m_socket = clientSocket;
+        }
+        byte[] datas;
+        /// <summary>
+        /// 开始接收客户端传入的数据
+        /// 1.接收消息
+        /// 2.解析数据
+        /// 3.处理数据
+        /// 4.响应客户端
+        /// </summary>
+        public void Start()
+        {
+            if (m_socket == null || m_socket.Connected == false) return;
+            m_socket.BeginReceive(datas, 0, 1024, SocketFlags.None, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+
+        }
+    }
+}
---
+++ XFrameworkServer/XFrameworkServer/Servers/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace XFrameworkServer.Servers
+{
+    /// <summary>
+    /// 服务器主类
+    /// 接受通信
+    /// </summary>
+    class Server
+    {
+        private IPEndPoint m_IPEndPoint;
+        private Socket m_serverSocket;
+
+        public Server() { }
+
+
+        /// <summary>
+        /// 传入IP和端口号即可构造
+        /// </summary>
+        /// <param name="ipStr"></param>
+        /// <param name="port"></param>
+        public Server(string ipStr, int port)
+        {
+            SetIPEndPoint(ipStr, port);
+        }
+
+        public void SetIPEndPoint(string ipstr, int port)
+        {
+            m_IPEndPoint = new IPEndPoint(IPAddress.Parse(ipstr), port);
+        }
+
+        /// <summary>
+        /// 开启TCP服务,开始监听客户端连接
+        /// </summary>
+        public void StartServer()
+        {
+            m_serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            if(m_IPEndPoint == null)
+            {
+                SetIPEndPoint("127.0.0.1", 8809);
+            }
+            m_serverSocket.Bind(m_IPEndPoint);
+            m_serverSocket.Listen(0);
+            m_serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        /// <summary>
+        /// 接收客户端请求
+        /// </summary>
+        /// <param name="ar"></param>
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = m_serverSocket.EndAccept(ar);
+            m_serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+    }
+}
